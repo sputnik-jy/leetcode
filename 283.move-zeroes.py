4ad25905c708b79a98fae7b80092d9b9e6cea3f2
jy <jinyee97@gmail.com>
2026-03-11 23:48:27 +0800

    feat: 283 using two pointer to solve in one go

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -9,17 +9,15 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
-        i = 0
-        j = len(nums) - 1
-        
-        if len(nums) == 1 or sum(nums) == 0:
-            return nums
-        
-        while i < j:
-            if nums[i] == 0:
-                while nums[j] == 0:
-                    j -= 1
-        
+        """                
+        slow = 0
+        fast = 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+            
+
 # @lc code=end
 
