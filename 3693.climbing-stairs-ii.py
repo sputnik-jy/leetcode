a1048ebdd8b36d79f671af0940f58dd0a2124101
jy <jinyee97@gmail.com>
2026-03-29 21:43:37 +0800

    refactor: format

--- 3693.climbing-stairs-ii.py
+++ 3693.climbing-stairs-ii.py
@@ -4,6 +4,7 @@
 # [3693] Climbing Stairs II
 #
 
+
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int, costs: List[int]) -> int:
@@ -16,26 +17,25 @@ class Solution:
 
         dp[0] = 0
         # only one step from 0 -> 1
-        dp[1] = costs[0] + 1 
+        dp[1] = costs[0] + 1
         # either one step from 0 -> 2 or two steps 0 -> 1 -> 2
-        dp[2] = min(dp[0] + costs[1] + 4, dp[1] + costs[1] + 1) 
-        
+        dp[2] = min(dp[0] + costs[1] + 4, dp[1] + costs[1] + 1)
+
         # n = 1, 2
         if n < 3:
-            return min(costs[1] + (2 - 0)**2, dp[1] + costs[1] + (2 - 1)**2)
+            return min(costs[1] + (2 - 0) ** 2, dp[1] + costs[1] + (2 - 1) ** 2)
 
         for i in range(3, n + 1):
             # either j - i = 3
-            # or j - i = 2 
+            # or j - i = 2
             # or j - i = 1
-            candidates = [
-                dp[i-1] + costs[i-1] + 1,
-                dp[i-2] + costs[i-1] + 4,
-                dp[i-3] + costs[i-1] + 9,
-            ]
-            dp[i] = min(candidates)
-        
-        return dp[n]
-        
-# @lc code=end
+            dp[i] = min(
+                dp[i - 1] + costs[i - 1] + 1,
+                dp[i - 2] + costs[i - 1] + 4,
+                dp[i - 3] + costs[i - 1] + 9,
+            )
 
+        return dp[n]
+
+
+# @lc code=end
